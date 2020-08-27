52cf713aec0c7b91e2869ce75f478c66291697cd
Davi A Souza <daviuai25@gmail.com>
2020-08-27 04:19:26 -0300

    Correção de overlap no gerador de chunks
    Corrigido os problemas de overlap por áreas já preenchidas, tanto de origem quanto por considerar os vizinhos, precisa de limpeza

---
+++ LittleAlchemist_UE4/Source/LittleAlchemist_UE4/LittleAlchemist_UE4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LittleAlchemist_UE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LittleAlchemist_UE4, "LittleAlchemist_UE4" );
---
+++ LittleAlchemist_UE4/Source/LittleAlchemist_UE4/Public/GetChildClasses.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GetChildClasses.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LITTLEALCHEMIST_UE4_API UGetChildClasses : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
